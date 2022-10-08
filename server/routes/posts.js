af55f867bc18cf1b34c19a2a5e6b996477846b6b
Mones Hamd <mones.m.azzam91@gmail.com>
2022-10-08 13:47:53 +0200

    initial delet method server side

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { getPosts, createPost, updatePost } from '../controllers/posts.js';
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} from '../controllers/posts.js';
 const router = express.Router();
 router.get('/api/posts', getPosts);
 router.get('/api/posts/:id', getPosts);
 router.post('/api/posts', createPost);
 router.put('/api/posts/:id', updatePost);
+router.delete('/api/posts/:id', deletePost);
 export default router;
