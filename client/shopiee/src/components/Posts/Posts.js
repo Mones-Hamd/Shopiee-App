7cd5eb8a718330a7d8bc6d023c267291a987030c
Mones Hamd <mones.m.azzam91@gmail.com>
2022-12-07 11:39:56 +0100

    update navbar style

--- client/shopiee/src/components/Posts/Posts.js
+++ client/shopiee/src/components/Posts/Posts.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 
 import { CircularProgress, Grid, Typography } from '@mui/material';
 import Post from './Post/Post';
